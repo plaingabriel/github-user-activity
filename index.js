027ab5a5d178775be6a5fca39c2ad7760d871ec4
Gabriel Prado <gaboalespana@gmail.com>
2025-10-06 22:13:58 -0400

    update

--- index.js
+++ index.js
@@ -1,9 +1,41 @@
-const username = "Juanperias";
+import chalk from "chalk";
+
+const username = process.argv[2];
+
+if (!username) {
+  console.log("Please provide a GitHub username as an argument.");
+  process.exit(1);
+}
+
 const url = `https://api.github.com/users/${username}/events`;
+
+function makeRed(string) {
+  return chalk.red(string);
+}
+
+function makeCharsRed(string, chars) {
+  let newString = "";
+
+  for (const char of string) {
+    if (chars.includes(char)) {
+      newString += makeRed(char);
+      continue;
+    }
+
+    newString += char;
+  }
+
+  return newString;
+}
 
 try {
   const response = await fetch(url);
   const events = await response.json();
+
+  if (events.status === "404") {
+    console.log("User not found");
+    process.exit(1);
+  }
 
   await Promise.all(
     events.map(async (event) => {
@@ -12,7 +44,7 @@ try {
       switch (event.type) {
         case "PushEvent":
           const commitCount = event.payload.commits.length;
-          action = `Pushed ${commitCount} ${
+          action = `Pushed ${chalk.cyan(commitCount)} ${
             commitCount === 1 ? "commit" : "commits"
           } to ${event.repo.name}`;
           break;
@@ -21,11 +53,13 @@ try {
           action = `${
             event.payload.action.charAt(0).toUpperCase() +
             event.payload.action.slice(1)
-          } a new issue in ${event.repo.name}`;
+          } a ${makeRed("new")} issue ${makeRed("in")} ${event.repo.name}`;
           break;
 
         case "CreateEvent":
-          action = `Created a new ${event.payload.ref_type} in ${event.repo.name}`;
+          action = `Created a ${makeRed("new")} ${
+            event.payload.ref_type
+          } ${makeRed("in")} ${event.repo.name}`;
           break;
 
         case "WatchEvent":
@@ -40,12 +74,18 @@ try {
           action = `Published ${event.repo.name}`;
           break;
 
+        case "IssueCommentEvent":
+          action = `Commented on an issue in ${event.repo.name}`;
+          break;
+
         default:
           action = `${event.type.replace(/Event$/, "")} on ${event.repo.name}`;
           break;
       }
 
-      console.log("- ", action);
+      const formatedAction = makeCharsRed(action, ["/", "-"]);
+
+      console.log("- ", formatedAction);
     })
   );
 } catch (error) {
